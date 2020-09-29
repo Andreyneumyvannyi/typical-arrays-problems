663fcd68a837c035623e3914d706e7bbf07ffa30
Andrey Neumyvannyi <andreyneumuvanny@gmail.com>
2020-09-29 12:59:37 +0400

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        let max = min;
+        for (i = 1; i < array.length; ++i) {
+            if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i];
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        let max = min;
+        for (i = 1; i < array.length; ++i) {
+            if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i];
+        }
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let count = 0;
+        for (let i = 0; i < array.length; i++) {
+            count += array[i];
+        }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return count / array.length;
+    }
+};
